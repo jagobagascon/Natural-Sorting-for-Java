bd64c6537c51f66ef524d8b44ac61c8549f131cd
saharNooby <saharnooby@protonmail.com>
2021-08-25 05:22:04 +0500

    Remove duplicated data
    Since we use randomized testing and test list is shuffled anyway, there is no need to provide manually unordered array.

--- test/com/github/jagobagascon/NaturalSortTest.java
+++ test/com/github/jagobagascon/NaturalSortTest.java
@@ -8,54 +8,34 @@ public class NaturalSortTest {
 
 	@Test
 	public void testStrings() {
-		List<String> test = Arrays.asList("f", "b", "c", "d", "e", "a");
-		List<String> expectedResult = Arrays.asList("a", "b", "c", "d", "e", "f");
-
-		testAndExpect(test, expectedResult);
+		testSort(Arrays.asList("a", "b", "c", "d", "e", "f"));
 	}
 
 	@Test
 	public void testList() {
-		List<String> test = Arrays.asList("Item no. 1", "Item no. 2", "Item no. 3", "Item no. 4", "Item no. 4",
-				"Item no. 6", "Item no. 7");
-		List<String> expectedResult = Arrays.asList("Item no. 1", "Item no. 2", "Item no. 3", "Item no. 4",
-				"Item no. 4", "Item no. 6", "Item no. 7");
-
-		testAndExpect(test, expectedResult);
+		testSort(Arrays.asList("Item no. 1", "Item no. 2", "Item no. 3", "Item no. 4",
+				"Item no. 4", "Item no. 6", "Item no. 7"));
 	}
 
 	@Test
 	public void testSameNumberDifferentAfterIt() {
-		List<String> test = Arrays.asList("Item 1A", "Item 1B", "Item 1C", "Item 2A", "Item 2B", "Item 2C");
-		List<String> expectedResult = Arrays.asList("Item 1A", "Item 1B", "Item 1C", "Item 2A", "Item 2B", "Item 2C");
-
-		testAndExpect(test, expectedResult);
+		testSort(Arrays.asList("Item 1A", "Item 1B", "Item 1C", "Item 2A", "Item 2B", "Item 2C"));
 	}
 
 	@Test
 	public void testIgnoreCase() {
-		List<String> test = Arrays.asList("Item 1A", "Item 1b", "Item 1C", "Item 2A", "Item 2B", "Item 2c");
-		List<String> expectedResult = Arrays.asList("Item 1A", "Item 1b", "Item 1C", "Item 2A", "Item 2B", "Item 2c");
-
-		testAndExpectIgnoreCase(test, expectedResult);
+		testSortIgnoreCase(Arrays.asList("Item 1A", "Item 1b", "Item 1C", "Item 2A", "Item 2B", "Item 2c"));
 	}
 
 	@Test
 	public void testEmptyStrings() {
-		List<String> test = Arrays.asList("00010", "0010", "010", "10", "001", "01", "1", "");
-		List<String> expectedResult = Arrays.asList("", "1", "01", "001", "10", "010", "0010", "00010");
-
-		testAndExpect(test, expectedResult);
+		testSort(Arrays.asList("", "1", "01", "001", "10", "010", "0010", "00010"));
 	}
 
 	@Test
 	public void testMixedValues() {
-		List<String> test = Arrays.asList("Item no. 1", "Item no. 01", "Item no. 10", "Item no. 2", "Item no. 3",
-				"Item no. 11", "Item no. 010");
-		List<String> expectedResult = Arrays.asList("Item no. 1", "Item no. 01", "Item no. 2", "Item no. 3",
-				"Item no. 10", "Item no. 010", "Item no. 11");
-
-		testAndExpect(test, expectedResult);
+		testSort(Arrays.asList("Item no. 1", "Item no. 01", "Item no. 2", "Item no. 3",
+				"Item no. 10", "Item no. 010", "Item no. 11"));
 	}
 
 	/**
@@ -64,29 +44,24 @@ public class NaturalSortTest {
 	 */
 	@Test
 	public void testEqualNumbersDifferentRepresentation() {
-		List<String> test = Arrays.asList("00010", "0010", "010", "10", "001", "01", "1", "0");
-		List<String> expectedResult = Arrays.asList("0", "1", "01", "001", "10", "010", "0010", "00010");
-
-		testAndExpect(test, expectedResult);
+		testSort(Arrays.asList("0", "1", "01", "001", "10", "010", "0010", "00010"));
 	}
 
 	@Test
 	public void testBigNumbers() {
-		List<String> test = Arrays.asList("123456789", "1", "123456789123456789123456789", "123456789123456789", "00000000000000000000000000000000000000000000000000000");
-		List<String> expectedResult = Arrays.asList("00000000000000000000000000000000000000000000000000000", "1", "123456789", "123456789123456789", "123456789123456789123456789");
-
-		testAndExpect(test, expectedResult);
+		testSort(Arrays.asList("00000000000000000000000000000000000000000000000000000",
+				"1", "123456789", "123456789123456789", "123456789123456789123456789"));
 	}
 
-	private void testAndExpect(List<String> test, List<String> expectedResult) {
-		testAndExpect(test, expectedResult, NaturalSort.naturalSortComparator);
+	private void testSort(List<String> expectedResult) {
+		testSort(expectedResult, NaturalSort.naturalSortComparator);
 	}
 
-	private void testAndExpectIgnoreCase(List<String> test, List<String> expectedResult) {
-		testAndExpect(test, expectedResult, NaturalSort.naturalSortIgnoreCaseComparator);
+	private void testSortIgnoreCase(List<String> expectedResult) {
+		testSort(expectedResult, NaturalSort.naturalSortIgnoreCaseComparator);
 	}
 
-	private void testAndExpect(List<String> test, List<String> expectedResult, Comparator<String> comparator) {
+	private void testSort(List<String> expectedResult, Comparator<String> comparator) {
 		// Calculate seed depending only on test data to make tests deterministic and failures reproducible
 		long seed = 0;
 
@@ -99,6 +74,8 @@ public class NaturalSortTest {
 		Random random = new Random(seed);
 
 		int runCount = 64;
+
+		List<String> test = new ArrayList<>(expectedResult);
 
 		for (int run = 0; run < runCount; run++) {
 			Collections.shuffle(test, random);
